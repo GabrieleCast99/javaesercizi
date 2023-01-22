c524ac1cd0f247577a6178a3a0b9cfc163e5a174
Gabriele <gabriele.castiglione@libero.it>
2023-01-22 20:30:28 +0100

    exercise 13--if...else1--

---
+++ es_10---19/esercizio_13--If Statement1--/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        String myName = "Gabriele";
+        if (myName.length() % 2 == 0) {
+            System.out.println("The name is even");
+        } else {
+            System.out.println("The name is odd");
+        }
+    }
+}
