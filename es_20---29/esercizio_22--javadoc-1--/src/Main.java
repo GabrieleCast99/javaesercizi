a2ed4be0c24f68f288288c11c95b84975bbeb96c
Gabriele <gabriele.castiglione@libero.it>
2023-01-25 18:57:38 +0100

    exercise 22--Java_doc_1--

---
+++ es_20---29/esercizio_22--javadoc-1--/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        WordGames pt1 = new WordGames();
+        pt1.addHelloWord("word");
+        pt1.getFullName("gabri","casti");
+    }
+}
---
+++ es_20---29/esercizio_22--javadoc-1--/src/WordGames.java
@@ -0,0 +1,22 @@
+/**
+*This class represents 2 strings
+*@author Gabriele
+ */
+public class WordGames {
+    /**
+     * This method stamp a string "Hello"+ input word
+     * @param word description of the input word
+     */
+    public void addHelloWord(String word) {
+        System.out.println("Hello " + word);
+    }
+
+    /**
+     * This method stamp the name and the surname of the user
+     * @param name the name of the user
+     * @param surname the surname of the user
+     */
+    public void getFullName(String name,String surname){
+        System.out.println(name+" "+surname);
+    }
+}
