d60813948a231ce62729f349c9cc98f944dafa9d
Gabriele <gabriele.castiglione@libero.it>
2023-01-30 13:27:46 +0100

    exercise_15--if_statement_3--

--- es_10---19/esercizio_15-- If Statement_3--/src/Main.java
+++ es_10---19/esercizio_15-- If Statement_3--/src/Main.java
@@ -2,17 +2,9 @@ import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
-        Random randomNumbers = new Random();
-        int value = randomNumbers.nextInt(35);
         Student student1=new Student("gabri",24);
-        System.out.println("I generated the random number "+value+" for student "+ student1.name+" age: "+student1.age);
 
-        if (value== student1.age){
-            System.out.println("the random value is equal age");
-        } else if (value< student1.age) {
-            System.out.println("the random value is less than age");
-        }else if (value> student1.age){
-            System.out.println("the random value is more than age");
-        }
+        student1.guessingAge();
+
     }
 }
--- es_10---19/esercizio_15-- If Statement_3--/src/Student.java
+++ es_10---19/esercizio_15-- If Statement_3--/src/Student.java
@@ -1,11 +1,28 @@
 import java.util.Random;
 public class Student {
-     String name;
-     int age;
+    String name;
+    int age;
 
-    public  Student(String studentName, int studentAge) {
-        this.name=studentName;
-        this.age=studentAge;
+    public Student(String studentName, int studentAge) {
+        this.name = studentName;
+        this.age = studentAge;
+
+
+
     }
 
+    public void guessingAge() {
+        Random randomNumbers = new Random();
+        int value = randomNumbers.nextInt(35);
+
+        System.out.println("I generated the random number " + value + " for student " + this.name + " age: " + this.age);
+
+        if (value == this.age) {
+            System.out.println("the random value is equal age");
+        } else if (value < this.age) {
+            System.out.println("the random value is less than age");
+        } else if (value > this.age) {
+            System.out.println("the random value is more than age");
+        }
+    }
 }
