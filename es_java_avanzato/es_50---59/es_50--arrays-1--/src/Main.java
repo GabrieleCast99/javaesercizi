8593e710393ea08a0d7c39621736c69d71bbf74a
Gabriele <gabriele.castiglione@libero.it>
2023-02-02 16:37:21 +0100

    exercise_50--array_1--

---
+++ es_java_avanzato/es_50---59/es_50--arrays-1--/src/Main.java
@@ -0,0 +1,12 @@
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        String[] carBrands={"Volvo","Opel","Mercedes","fiat","BMW"};
+        System.out.println(carBrands.length);
+        System.out.println(carBrands[2]);
+        int[]primeNumbers =new int[]{2, 3, 5, 7, 11, 13};
+        System.out.println(Arrays.toString(primeNumbers));
+    }
+}
