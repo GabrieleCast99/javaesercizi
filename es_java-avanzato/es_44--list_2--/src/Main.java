80edefa414f31fe67187bb53dab0f458e9546fbb
Gabriele <gabriele.castiglione@libero.it>
2023-02-05 18:10:31 +0100

    exercise_44--list_2--

---
+++ es_java-avanzato/es_44--list_2--/src/Main.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Integer> divisorsOf52= new ArrayList<Integer>();
+        divisorsOf52.add(1);
+        divisorsOf52.add(2);
+        divisorsOf52.add(4);
+        divisorsOf52.add(13);
+        divisorsOf52.add(26);
+        divisorsOf52.add(52);
+
+        List<Integer> randomNumbers=new ArrayList<Integer>();
+        randomNumbers.add(1);
+        randomNumbers.add(60);
+        randomNumbers.add(13);
+        randomNumbers.add(12);
+
+
+        randomNumbers.addAll(4,divisorsOf52);
+        System.out.println(randomNumbers);
+        System.out.println("----------");
+
+
+        randomNumbers.remove(2);
+        System.out.println(randomNumbers);
+        randomNumbers.remove(3);
+        System.out.println(randomNumbers);
+        System.out.println(randomNumbers.size());
+        System.out.println("----------");
+
+        Collections.sort(randomNumbers);
+        System.out.println(randomNumbers);
+    }
+}
